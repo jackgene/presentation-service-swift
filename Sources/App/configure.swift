6d6f3ab34fd4887eebcfa17a8f7018b462896125
Jack Leow <jackgene@gmail.com>
2022-07-15 09:54:15 -0700

    Initial Vapor-based implementation.

---
+++ Sources/App/configure.swift
@@ -0,0 +1,12 @@
+import Vapor
+
+// configures your application
+public func configure(_ app: Application) throws {
+    app.http.server.configuration.port = 8973
+
+    // uncomment to serve files from /Public folder
+    // app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
+
+    // register routes
+    try routes(app)
+}
