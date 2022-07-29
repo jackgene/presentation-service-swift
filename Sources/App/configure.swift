8621b8e39d4cb65d5c9ea78be449ba360fa2fb99
Jack Leow <jleow@goodrx.com>
2022-07-29 15:55:41 -0700

    Simplified HTTP server config creation.

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -4,8 +4,7 @@ import Vapor
 public func configure(_ app: Application) throws {
     app.commands.use(PresentationServiceCommand(), as: "present", isDefault: true)
 
-    app.http.server.configuration.hostname = "0.0.0.0"
-    app.http.server.configuration.port = 8973
+    app.http.server.configuration = .init(hostname: "0.0.0.0", port: 8973)
 
     // uncomment to serve files from /Public folder
     // app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
