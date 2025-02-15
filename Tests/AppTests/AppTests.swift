fdd8a77ca096548ee4e050d55fcc7c316f17b6ec
Jack Leow <jackgene@gmail.com>
2025-02-15 14:40:44 -0800

    Explicitly shutdown PresentationServiceCommand in test.

--- Tests/AppTests/AppTests.swift
+++ Tests/AppTests/AppTests.swift
@@ -3,25 +3,44 @@ import SwiftHamcrest
 import XCTVapor
 
 final class AppTests: XCTestCase {
-    func testLoadModerator() throws {
-        let app = Application(.testing)
-        defer { app.shutdown() }
-        try configure(app)
-        
-        try app.test(.GET, "/moderator", afterResponse: { res in
-            assertThat(res.status, equalTo(.ok))
-            assertThat(res.body.string, containsString("<title>Moderator</title>"))
-        })
+    private func withApp(_ test: (Application) throws -> ()) async throws {
+        let app = try await Application.make(.testing)
+        do {
+            try await configure(app)
+            try test(app)
+        }
+        catch {
+            app.asyncCommands.commands.forEach {
+                if let presentationServiceCmd = $0.1 as? PresentationServiceCommand {
+                    presentationServiceCmd.shutdown()
+                }
+            }
+            try await app.asyncShutdown()
+            throw error
+        }
+        app.asyncCommands.commands.forEach {
+            if let presentationServiceCmd = $0.1 as? PresentationServiceCommand {
+                presentationServiceCmd.shutdown()
+            }
+        }
+        try await app.asyncShutdown()
     }
     
-    func testLoadTranscriber() throws {
-        let app = Application(.testing)
-        defer { app.shutdown() }
-        try configure(app)
-
-        try app.test(.GET, "/transcriber", afterResponse: { res in
-            assertThat(res.status, equalTo(.ok))
-            assertThat(res.body.string, containsString("<title>Transcriber</title>"))
-        })
+    func testLoadModerator() async throws {
+        try await withApp { app in
+            try app.test(.GET, "/moderator", afterResponse: { res in
+                assertThat(res.status, equalTo(.ok))
+                assertThat(res.body.string, containsString("<title>Moderator</title>"))
+            })
+        }
+    }
+    
+    func testLoadTranscriber() async throws {
+        try await withApp { app in
+            try app.test(.GET, "/transcriber", afterResponse: { res in
+                assertThat(res.status, equalTo(.ok))
+                assertThat(res.body.string, containsString("<title>Transcriber</title>"))
+            })
+        }
     }
 }
